ee347b31df0dea1a11ae4555d4c1b7d1909c7490
ThiagoBryan <tthurler10@gmail.com>
2022-04-25 23:01:47 -0300

    atualização

---
+++ POO/src/br/com/curso/aula13/Animal.java
@@ -0,0 +1,43 @@
+package br.com.curso.aula13;
+
+public abstract class Animal {
+
+	
+	//ATRIBUTOS
+			private double peso;
+			private Integer idade;
+			private Integer membros;
+			
+			//GETTERS AND SETTERS
+			
+			public double getPeso() {
+				return peso;
+			}
+
+			public void setPeso(double peso) {
+				this.peso = peso;
+			}
+
+			public Integer getIdade() {
+				return idade;
+			}
+
+			public void setIdade(Integer idade) {
+				this.idade = idade;
+			}
+
+			public Integer getMembros() {
+				return membros;
+			}
+
+			public void setMembros(Integer membros) {
+				this.membros = membros;
+			}
+
+			//METODOS
+			public abstract void locomover();
+			
+			public abstract void alimentar();
+			
+			public abstract void emitirSom();
+}
